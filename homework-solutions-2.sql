ae32504546d0edbf45a2a6a9b3b6bc9d21845a1a
volkan-terzi-tr <114071071+volkan-terzi-tr@users.noreply.github.com>
2023-10-28 18:42:41 +0300

    I added my 2st homework in education and its solution.

---
+++ homework-solutions-2.sql
@@ -0,0 +1,10 @@
+SELECT * FROM film
+WHERE replacement_cost BETWEEN 12.99 AND 16.99;          -- 1.soru
+
+
+SELECT first_name, last_name FROM actor
+WHERE first_name IN('Penelope', 'Ed', 'Nick');           -- 2.soru
+
+
+SELECT * FROM film
+WHERE rental_rate IN(0.99, 2.99, 4.99) AND replacement_cost IN(12.99, 15.99, 28.99);       -- 3.soru
